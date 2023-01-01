d5fec4b5bd75d099da534cf80c49576800cfbf8b
mylesthecoder <mylesknjenga@gmail.com>
2023-01-01 02:44:20 -0500

    footer responsivity

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -6,12 +6,12 @@ export const Footer = () => {
     return (
         <Container fluid className="footer">
             <Row>
-                <Col className="py-4 d-flex align-items-center justify-content-center">TechBaseLogo</Col>
-                <Col className="d-flex align-items-center justify-content-center">&copy; copyright</Col>
-                <Col className="d-flex align-items-center justify-content-center">
-                    <FontAwesomeIcon className="me-3" icon={faFacebook} />
-                    <FontAwesomeIcon className="me-3" icon={faInstagram} />
-                    <FontAwesomeIcon className="me-3" icon={faTwitter} />
+                <Col xs={12} md={4} className="py-4 d-flex align-items-center justify-content-center">TechBaseLogo</Col>
+                <Col xs={12} md={4} className="py-4 d-flex align-items-center justify-content-center">&copy; copyright</Col>
+                <Col xs={12} md={4} className="py-4 d-flex align-items-center justify-content-center">
+                    <FontAwesomeIcon className="me-2" icon={faFacebook} />
+                    <FontAwesomeIcon className="me-2" icon={faInstagram} />
+                    <FontAwesomeIcon className="me-2" icon={faTwitter} />
                 </Col>
             </Row>
         </Container>
