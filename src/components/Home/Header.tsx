8553890c2e62373e207bc16989e3b374560ab035
mylesthecoder <mylesknjenga@gmail.com>
2023-01-02 19:57:35 -0500

    removed unused import

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, Row, Col, Button } from 'react-bootstrap';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
